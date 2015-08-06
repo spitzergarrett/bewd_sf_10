1c11a5f5c244050cf8ba6353a7ee9c2ce67ebc87
Garrett Spitzer <spitzer@squareup.com>
2015-08-05 19:42:27 -0700

    reverse solution explained

---
+++ 03_Ruby_Fundamentals/solutions/reverse_it_my_way.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+def my_reverse(string)
+  char = string.downcase.chars #transform string into array of characters
+  word = "" #create an empty string for new word
+  until char.length == 0
+    word << char.pop #removes last element in array && returns letter
+  end
+  word #this is what will be returned because it's the last line
+end
+
+def is_palindrome?(word)
+  if word.downcase.strip == my_reverse(word).downcase
+    "YES!! Palindrome!!"
+  else
+    "Awww. Not quite a Palindrome"
+  end
+end
+
+#call our method here
+  puts "Give me word \n"
+  word = gets.strip
+
+  puts is_palindrome?(word)
